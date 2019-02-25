88f2716e1bada1ac73883569f23e44ec153413ae
Jan Loeffler <jan.loeffler21@gmail.com>
2019-02-25 19:57:14 +0100

    update

--- start.sh
+++ start.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-
+count=`ls -1 *.ovpn 2>/dev/null | wc -l`
 service ssh restart
-cd home/
+if [ $count != 0 ]
+then 
+
 for FILE in /*.ovpn; 
 do 
     echo ${FILE}
-    openvpn --config ${FILE}
+    openvpn --config ${FILE} &
     break; 
 done
+fi 
+cd home/
 
 jupyter-notebook --ip=0.0.0.0 --allow-root &
 
